7d7799d2d0861f2f48ff66653bdf428ebe187826
Sebastian <sebaorellana_2010@hotmail.com.ar>
2023-01-02 14:03:13 -0300

    Pagina principal,y pagina de historia con video incluido

---
+++ JS/script-historia.js
@@ -0,0 +1,22 @@
+let botonPlay = document.getElementById("play")
+let botonPausa = document.getElementById("pause")
+let video = document.querySelector("video")
+
+botonPlay.addEventListener("click", ()=> {
+    video.play()
+})
+
+botonPausa.addEventListener("click", ()=> {
+    video.pause()
+})
+
+//funcion que convierte el tiempo del video para que luego se pueda mostrar en el formato minutos:segundos
+//Ejemplo: intTommss(281.10) -> [04,41]
+function intTommss(tiempo) {
+    mm = parseInt(tiempo/60);
+    ss = parseInt(tiempo-(mm*60));
+    return ([mm,ss])
+}
+
+let duracionVideo = document.getElementById("duracion_video")
+duracionVideo.textContent = (intTommss(video.duration)).join(":")
